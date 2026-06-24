b811ec9d7524810bae4c0be1e8b6547d90b1b697
husanswe <sulaymonovhusan2002@gmail.com>
2026-06-24 13:21:41 +0500

    doing eloquent orm/models coding tasks on laravel tinker

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    protected $fillable = ['title', 'done'];
+    protected $fillable = ['title', 'done', 'priority'];
 
     protected $casts = ['done' => 'boolean'];
 }
