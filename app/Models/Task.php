8b809a1cc03e351d84bd52db7056c98b870775e9
husanswe <sulaymonovhusan2002@gmail.com>
2026-06-24 20:18:31 +0500

    completed task 6

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -9,4 +9,9 @@ class Task extends Model
     protected $fillable = ['title', 'done', 'priority'];
 
     protected $casts = ['done' => 'boolean'];
+
+    public function scopeCompleted($query)
+    {
+        return $query->where('done', true);
+    }
 }
