e63f496f8cef9879f7a4c200bb265ecb60c5280e
husanswe <sulaymonovhusan2002@gmail.com>
2026-06-27 19:15:29 +0500

    uncomment resource route and update test transaction route in web.php

--- routes/web.php
+++ routes/web.php
@@ -9,9 +9,25 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-/* Route::resource('tasks', TaskController::class);
+Route::resource('tasks', TaskController::class);
 
-Route::get('/test-raw', function() {
+/* Route::get('/test-raw', function() {
     $results = DB::select('SELECT * FROM tasks WHERE priority = ?', [2]);
     return $results;
 }); */
+
+Route::get('/test-transaction', function() {
+    DB::transaction(function() {
+        DB::table('tasks')->insert([
+            'title' => 'going to New York',
+            'done' => 0,
+            'priority' => 1,
+        ]);
+
+        DB::table('categories')->where('id', 1)->update([
+            'name' => 'Lorem ipsum',
+        ]);
+    });
+
+    return "Transaction completed";
+});
