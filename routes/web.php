6b82a30a6ce6b48f8b961624e11d0869081f4faa
husanswe <sulaymonovhusan2002@gmail.com>
2026-06-27 18:42:58 +0500

    comment out resource route and add test route for task priority query

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 use Laravel\Prompts\Task;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('tasks', TaskController::class);
+/* Route::resource('tasks', TaskController::class);
+
+Route::get('/test-raw', function() {
+    $results = DB::select('SELECT * FROM tasks WHERE priority = ?', [2]);
+    return $results;
+}); */
