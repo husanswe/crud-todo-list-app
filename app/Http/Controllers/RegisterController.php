19e8ae23f885da69127674b00cea70fb15b5ea94
husanswe <sulaymonovhusan2002@gmail.com>
2026-07-01 05:42:59 +0500

    added custom validation error messages to validation function

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -12,11 +12,19 @@ class RegisterController extends Controller
 
     public function register(Request $request) {
         $validated = $request->validate([
-            'name' => "required|string|max:50",
+            'name' => "required|string|min:3|max:50",
             'email'=> "required|email|unique:users,email",
             'age' => "required|integer|min:12",
             'password' => "required|string|min:8|confirmed",
             'website' => "nullable|url"
+        ], 
+        [
+            'name.required' => 'Please tell us your name.',
+            'name.main' => 'Name is too short - at least 3 characters.',
+            'email.email' => "That email doesn't look valid.",
+            'email.unique' => "That email has been already registered. Try logging in instead.",
+            'age.min' => 'You must be 12 or older to register',
+            'password.confirmed' => "Passwords don't match. Try again!"
         ]);
 
         dd($validated);
