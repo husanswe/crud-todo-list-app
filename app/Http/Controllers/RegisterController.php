6df5f56abcd566d70b810cbd8762e4d9240311c8
husanswe <sulaymonovhusan2002@gmail.com>
2026-07-01 06:06:19 +0500

    moved valide function to separate registerRequest file

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\RegisterRequest;
 
 class RegisterController extends Controller
 {
@@ -10,22 +11,9 @@ class RegisterController extends Controller
         return view('register-form');
     }
 
-    public function register(Request $request) {
-        $validated = $request->validate([
-            'name' => "required|string|min:3|max:50",
-            'email'=> "required|email|unique:users,email",
-            'age' => "required|integer|min:12",
-            'password' => "required|string|min:8|confirmed",
-            'website' => "nullable|url"
-        ], 
-        [
-            'name.required' => 'Please tell us your name.',
-            'name.main' => 'Name is too short - at least 3 characters.',
-            'email.email' => "That email doesn't look valid.",
-            'email.unique' => "That email has been already registered. Try logging in instead.",
-            'age.min' => 'You must be 12 or older to register',
-            'password.confirmed' => "Passwords don't match. Try again!"
-        ]);
+    public function register(RegisterRequest $request) 
+    {
+        $validated = $request->validated();
 
         dd($validated);
     }
---
+++ app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    // @return array<string, ValidationRule|array<mixed>|string>
+
+    public function rules(): array
+    {
+        return [
+            'name' => "required|string|min:3|max:50",
+            'email'=> "required|email|unique:users,email",
+            'age' => "required|integer|min:12|max:100",
+            'password' => "required|string|min:8|confirmed",
+            'website' => "nullable|url"
+        ];
+    }
+
+    public function messages() 
+    {
+        [
+            'name.required' => 'Please tell us your name.',
+            'name.min' => 'Name is too short - at least 3 characters.',
+            'email.email' => "That email doesn't look valid.",
+            'email.unique' => "That email has been already registered. Try logging in instead.",
+            'age.min' => 'You must be 12 or older to register',
+            'password.confirmed' => "Passwords don't match. Try again!"
+        ];
+    }
+}
