7342a0e8a8602db550446faff4e8c867b670cf2f
husanswe <sulaymonovhusan2002@gmail.com>
2026-07-10 21:56:54 +0500

    changed row of code in authcontroller.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,9 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AuthController extends Controller
 {
