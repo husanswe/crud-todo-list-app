7512cdc809a289176c17c227d70dcd75a43804bd
husanswe <sulaymonovhusan2002@gmail.com>
2026-07-10 21:55:06 +0500

    Fix password validation typo and enhance user registration logic in AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -13,9 +16,21 @@ class AuthController extends Controller
 
     public function register_store(Request $request)
     {
-        $validated = $request -> validate([
-
+        $validated = $request->validate([
+            'name' => 'required|string',
+            'email'=> 'email|required|unique:users',
+            'passowrd'=>'required|string|min:8'
         ]);
+
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password'])
+        ]);
+
+        Auth::login($user);
+
+        return redirect()->route('tasks.index');
     }
 
     public function showLogin()
