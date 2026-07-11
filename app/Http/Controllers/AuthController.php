b33f52b13d13deb1dc670ea77fd23e8737041161
husanswe <sulaymonovhusan2002@gmail.com>
2026-07-11 12:14:53 +0500

    wrote login logic

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use Illuminate\Http\RedirectResponse;
 
 class AuthController extends Controller
 {
@@ -19,7 +20,7 @@ class AuthController extends Controller
         $validated = $request->validate([
             'name' => 'required|string|max:40',
             'email'=> 'email|required|unique:users',
-            'password'=>'required|string|min:8'
+            'password'=>'required|confirmed|string|min:8'
         ]);
 
         $user = User::create([
@@ -38,9 +39,22 @@ class AuthController extends Controller
         return view('auth.login');
     }
 
-    public function login()
+    public function login(Request $request): RedirectResponse
     {
-        // 
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required']
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('tasks');
+        }
+
+        return back()->withErrors([
+            'email' => "The provided credentials don't match our records! Try again.",
+        ])->onlyInput('email');
     }
 
     public function logout()
