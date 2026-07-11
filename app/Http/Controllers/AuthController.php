8e71015137758baf322ed33bc353055f0d099c4d
husanswe <sulaymonovhusan2002@gmail.com>
2026-07-11 12:52:51 +0500

    wrote logout logic

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -57,8 +57,14 @@ class AuthController extends Controller
         ])->onlyInput('email');
     }
 
-    public function logout()
+    public function logout(Request $request)
     {
-        
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
     }
 }
