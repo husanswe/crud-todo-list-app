07f074e61b5eb22f87f4463dd5bc0f50a656c633
husanswe <sulaymonovhusan2002@gmail.com>
2026-07-20 19:21:13 +0500

    added import button

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -16,6 +16,9 @@
             class="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-lg shadow">
                 + New Task
             </a>
+            <a href="{{ route('tasks.import.show') }}" class="bg-green-600 hover:bg-green-700 text-white px-4 py-2 rounded-lg shadow">
+                Import
+            </a>
         </div>
 
         @if(session('success'))
